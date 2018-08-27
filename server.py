ee756f594bdf79817bba706d5c5a4a85a2afc7df
Evgeni <semenchuk-e@inbox.ru>
2018-08-27 22:57:37 +0300

    логика сервера переписана, теперь он неблокирующий

--- server.py
+++ server.py
@@ -9,56 +9,107 @@
 """
 
 import sys
-import json
+import select
 from socket import socket, AF_INET, SOCK_STREAM
-from jim.utils import dict_to_bytes, bytes_to_dict, send_message, get_message
+from jim.utils import send_message, get_message
 from jim.config import *
+from jim.jim_main import JimResponse, JimMessage, Jim
 
-def presence_response(presence_message):
-    """
-    формирование ответа клиенту по его сообощению
-    :param presence_message: словарь (presence) запроса клиента
-    :return: словарь ответа
-    """
-    #проверяем что нам пришло presence сообщение
-    #если все ок, шлёи код ответа 200 иначе 400
 
-    if ACTION in presence_message and \
-            presence_message[ACTION] == PRESENCE and \
-            TIME in presence_message and \
-            isinstance(presence_message[TIME], float):
-        return {RESPONSE: 200,
-                MESSAGE: 'Hello client'}
-    else:
-        return {RESPONSE: 400, ERROR: 'был получен не верный запрос'}
+class Handler:
+    """
+    основная логика сервера
+    """
+    def read_requests(self, r_clients, all_clients):
+        # список входящих сообщений
+        messages = []
+
+        for sock in r_clients:
+            try:
+                message = get_message(sock)
+                messages.append(message)
+            except:
+                print('Клиент {} {} отключился'. format(sock.fileno(), sock.getpeername()))
+                all_clients.remove(sock)
+
+        return messages
+
+    def write_responses(self, messages, w_clients, all_clients):
+        for sock in w_clients:
+            for message in messages:
+                try:
+                    send_message(sock, message)
+                except:
+                    print('Клиент {} {} отключился'.format(sock.fileno(), sock.getpername()))
+                    sock.close()
+                    all_clients.remove(sock)
+
+    def presence_response(self, jim_message):
+        try:
+            Jim.from_dict(jim_message)
+        except Exception as e:
+            response = JimResponse(400, error=str(e))
+            return response.to_dict()
+        else:
+            response = JimResponse(200)
+            return response.to_dict()
+
+
+class Server:
+    def __init__(self, handler):
+        self.handler = handler
+        self.clients = []
+        self.sock = socket(AF_INET, SOCK_STREAM)
+
+    def bind(self, addr, port):
+        self.sock.bind((addr, port))
+
+    def listen_forever(self):
+        self.sock.listen(15)
+        self.sock.settimeout(0.2)
+
+        while True:
+            try:
+                client, addr = self.sock.accept()
+                presence = get_message(client)
+                print('вот что пришло на сервер ->', presence)
+                response = self.handler.presence_response(presence)
+                print('вот что отдаём с сервера ->', response)
+                send_message(client, response)
+            except OSError as e:
+                pass
+            else:
+                print('Получен запрос на соединение от клиента с адресом {}'.format(str(addr)))
+                self.clients.append(client)
+            finally:
+                wait = 0
+                r = []
+                w = []
+                try:
+                    r, w, e = select.select(self.clients, self.clients, [], wait)
+                except:
+                    pass
+
+                requests = self.handler.read_requests(r, self.clients)
+                self.handler.write_responses(requests, w, self.clients)
+
+
 
 
 if __name__ == '__main__':
-    server = socket(AF_INET, SOCK_STREAM)
-
     try:
         addr = sys.argv[1]
     except IndexError:
-        addr = 'localhost'
+        addr = ''
     try:
-        port = sys.argv[2]
+        port = int(sys.argv[2])
     except IndexError:
         port = 7777
     except ValueError:
-        print('Номер порта должен быть целым числом')
+        print('Порт должен быть целым числом')
         sys.exit(0)
 
-    server.bind((addr, port))
-    server.listen(10)
-    while True:
-        client, addr = server.accept()
-        #получаем сообщение от клиента
-        presence = get_message(client)
-        #печатаем сообщение
-        print(presence)
-
-        #формируем ответ и отсылаем его
-        responce = presence_response(presence)
-        send_message(client, responce)
-        client.close()
-
+    handler = Handler()
+    server = Server(handler)
+    server.bind(addr, port)
+    server.listen_forever()
