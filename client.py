a78d03a0fc1cf7a7ee7ef1128c5248c7ee3594e3
Evgeni <semenchuk-e@inbox.ru>
2018-08-27 18:46:39 +0300

    логика клиента переписана в соответствии с протоколом jim

--- client.py
+++ client.py
@@ -14,62 +14,37 @@ from socket import socket, AF_INET, SOCK_STREAM
 from errors import UserNameToLongError, ResponseCodeLenError, MandatoryKeyError, ResponseCodeError
 from jim.config import *
 from jim.utils import send_message, get_message
+from jim.jim_main import JimPresence, JimMessage, Jim, JimResponse
 
+class User:
+    def __init__(self, username):
+        self.username = username
 
-def create_presence(account_name="Guest"):
-    """
-    Сформировать ​​presence-сообщение
-    :param account_name: Имя пользователя
-    :return: Словарь сообщения
-    tests:
-    ИЗ-ЗА времени трудно написать doctest
-    """
-    # Если имя не строка
-    if not isinstance(account_name, str):
-        # Генерируем ошибку передан неверный тип
-        raise TypeError
-    # Если длина имени пользователя больше 25 символов
-    if len(account_name) > 26:
-        # генерируем нашу ошибку имя пользователя слишком длинное
-        raise UserNameToLongError(account_name)
-    # формируем словарь сообщения
-    message = {
-        ACTION: PRESENCE,
-        TIME: time.time(),
-        USER: {
-            ACCOUNT_NAME: account_name
-        },
-        MESSAGE: 'Hello server'
-    }
-    # возвращаем
-    return message
+    def create_presence(self):
+        jim_presence = JimPresence(self.username)
+        message = jim_presence.to_dict()
+        return message
 
+    def look_at_response(self, response):
+        result = Jim.from_dict(response)
+        return result.to_dict()
 
-def translate_message(response):
-    """
-    Разбор сообщения
-    :param response: Словарь ответа от сервера
-    :return: корректный словарь ответа
-    """
-    # Передали не словарь
-    if not isinstance(response, dict):
-        raise TypeError
-    # Нету ключа response
-    if RESPONSE not in response:
-        # Ошибка нужен обязательный ключ
-        raise MandatoryKeyError(RESPONSE)
-    # получаем код ответа
-    code = response[RESPONSE]
-    # длина кода не 3 символа
-    if len(str(code)) != 3:
-        # Ошибка неверная длина кода ошибки
-        raise ResponseCodeLenError(code)
-    # неправильные коды ответа
-    if code not in RESPONSE_CODES:
-        # ошибка неверный код ответа
-        raise ResponseCodeError(code)
-    # возвращаем ответ
-    return response
+    def create_message(self, message_to, text):
+        message = JimMessage(message_to, self.username, text)
+        message.to_dict()
+
+    def read_messages(self, sock):
+        while True:
+            print('читаю')
+            jim_message = get_message(sock)
+            print(jim_message)
+            print(jim_message[MESSAGE])
+
+    def write_messages(self, sock):
+        while True:
+            text = input('>> Отправить: ')
+            jim_message = self.create_message('#all', text)
+            send_message(sock, jim_message)
 
 
 if __name__ == '__main__':
@@ -86,14 +61,26 @@ if __name__ == '__main__':
     except ValueError:
         print('Порт должен быть целым числом')
         sys.exit(0)
+    try:
+        mode = sys.argv[3]
+    except IndexError:
+        mode = 'r'
     # Соединиться с сервером
     client.connect((addr, port))
+    # создаем пользователя
+    user = User('Nick Strong')
     # Создаем сообщение
-    presence = create_presence('Nick Strong')
+    presence = user.create_presence()
     # Отсылаем сообщение
     send_message(client, presence)
     # Получаем ответ
     response = get_message(client)
     # Проверяем ответ
-    response = translate_message(response)
-    print(response)
+    response = user.look_at_response(response)
+    if response[RESPONSE] == OK:
+        if mode == 'r':
+            user.read_messages(client)
+        elif mode == 'w':
+            user.write_messages(client)
+        else:
+            raise Exception('Неверный режим чтения / записи')
